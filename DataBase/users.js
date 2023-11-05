cad7c3d87a4003b41498b6a0643121164e03e039
ArtemMaidanuk <maidanukartem@gmail.com>
2023-11-06 01:46:35 +0300

    controler

---
+++ DataBase/users.js
@@ -0,0 +1,12 @@
+
+
+module.exports = [
+    {name:'Artem'},
+    {name:'Diana'},
+    {name:'Bodia'},
+    {name:'Dima'},
+    {name:'Katya'},
+    {name:'Anna'},
+    {name:'Oleg'},
+    {name:'Sasha'},
+]
